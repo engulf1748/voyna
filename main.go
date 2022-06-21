c956d448bc1876af47bfd6fb7c5210cbecc862e7
Ajay R <ar324@protonmail.com>
2022-06-21 11:41:05 +0530

    main.go: log (and print to stdout) number of goroutines each second

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"sync"
+	"fmt"
+	"runtime"
+	//"sync"
+	"time"
 
+	"codeberg.org/voyna/voyna/log4j"
 	"codeberg.org/voyna/voyna/processor"
 	"codeberg.org/voyna/voyna/server"
 )
@@ -11,10 +15,16 @@ func main() {
 	urls := tierOneURLs()
 	go processor.Process(urls)
 
-	server.Start()
+	go server.Start()
 
+	for {
+		s := fmt.Sprintf("NumGoroutine: %d\n", runtime.NumGoroutine())
+		log4j.Logger.Print(s)
+		fmt.Print(s)
+		time.Sleep(time.Second)
+	}
 	// TODO: remove the following
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Wait() // waits indefinitely
+	// 	var wg sync.WaitGroup
+	// 	wg.Add(1)
+	// 	wg.Wait() // waits indefinitely
 }
